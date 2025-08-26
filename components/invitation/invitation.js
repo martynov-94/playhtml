d4ef11616d884aacbeb6a57efc1cfc1137b23630
martynov-94 <martynovsg94@gmail.com>
2025-08-26 21:20:05 +0300

    day 4

--- components/invitation/invitation.js
+++ components/invitation/invitation.js
@@ -4,8 +4,8 @@ class Invitation extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <section class="invitation">
-        <h1 class="invitation__title">Хотите научиться web-разработке?</h1>
-        <p class="invitation__welcome-text">Приглашаем Вас<br>на бесплатный вебинар<br>«Тренды Frontend-разработки 2020»</p>
+        <h1 class="invitation__title">Хотите научиться<br> web-разработке?</h1>
+        <p class="invitation__welcome-text">Приглашаем Вас<br> на бесплатный вебинар «Тренды Frontend-разработки 2020»</p>
         <date-card class="invitation__date"></date-card>
         <img class="invitation__image" src="${invitationFolderPath}/img/man_with_laptop.png" width="256" height="234" alt="Человек сидит в позе лотоса с ноутбуком и проводит вебинар">
       </section>
